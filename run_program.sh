e18916d93d6879efc0d86a9e8fc034e06793d6fc
Lukas Woodtli <woodtli.lukas@gmail.com>
2017-11-24 15:02:15 +0100

    Fix example to run with qt5 and python3 on macOS

---
+++ run_program.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# this needs to be ran only once
+pyuic5 -o hauptdialog.py hauptdialog.ui
+
+./program.py
--- run_pyuic.bat
+++
@@ -1,2 +0,0 @@
-:: C:\Python26\Lib\site-packages\PyQt4\pyuic4 -o hauptdialog.py hauptdialog.ui
-pyuic5 -o hauptdialog.py hauptdialog.ui
